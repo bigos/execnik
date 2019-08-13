e22cd51c7daa5961a0612af775e4d4dd602d8d32
Jacek Podkanski <ruby.object@googlemail.com>
2019-08-13 18:10:04 +0100

    example works

--- compilation.lisp
+++ compilation.lisp
@@ -5,24 +5,12 @@
  asdf:*central-registry*)
 
 (ql:quickload :execnik)
-;; (in-package :execnik)
+(in-package :execnik)
 
 ;; ;; (sb-ext:disable-debugger)
 
-;; (sb-ext:save-lisp-and-die
-;;  #p "c:/Users/Jacek/Documents/Programming/Lisp/execnik/execnik.exe"
-;;  :toplevel #'run
-;;  :executable T
-;;  :application-type :console
-;;  )
-
-(require :asdf)
-
-(defmethod asdf:output-files ((o asdf:program-op)
-                              (s (eql (asdf:find-system :execnik))))
-  (let ((exe-path (uiop/os:getcwd)))
-    (if exe-path
-        (values (list (concatenate 'string (directory-namestring exe-path) "execnik2.exe")) t)
-        (call-next-method))))
-
-(asdf:operate :program-op :execnik)
+(sb-ext:save-lisp-and-die
+ #p "c:/Users/Jacek/Documents/Programming/Lisp/execnik/execnik.exe"
+ :toplevel #'run
+ :executable T
+ :application-type :gui)
--- execnik.lisp
+++ execnik.lisp
@@ -4,29 +4,20 @@
 (in-package #:execnik)
 
 (defun run ()
-  (format t "running RUN~%")
-
   (sb-int:with-float-traps-masked (:divide-by-zero)
-    (format t "a ~%")
     (within-main-loop
-      (format t "b ~%")
-      (let ((window (progn
-                      (format t "going to build window~%")
-                      (make-instance 'gtk-window :title "Execnik-window"
-                                                 :default-width 580
-                                                 :default-height 200
-                                                 :border-width 12
-                                                 :type :toplevel)))
-            (zzz (progn (format t "after window building~%"))))
-        (format t "window is ~A~%" window)
+      (let ((window (make-instance 'gtk-window :title "Execnik-window"
+                                               :default-width 580
+                                               :default-height 200
+                                               :border-width 12
+                                               :type :toplevel)))
 
         (g-signal-connect window "destroy"
                           (lambda (widget)
                             (declare (ignorable widget))
                             (format t "QUITTING~%")
                             (leave-gtk-main)))
-        (format t "c ~%")
-        (gtk-widget-show-all window)
-        (format t "d ~%"))))
 
-  (format t "after RUN~%"))
+        (gtk-widget-show-all window)))
+    ;; compiled program will not work on Windows without the following
+    (join-gtk-main)))
