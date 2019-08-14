466b6684ada38600e40905537c877015d548e5b6
Jacek Podkanski <ruby.object@googlemail.com>
2019-08-14 10:32:49 +0100

    compilation file refinements

--- compilation.lisp
+++ compilation.lisp
@@ -1,30 +1,27 @@
 ;;; sbcl --load THIS-FILE
 
-(defparameter project-path
-  (uiop/os:getcwd))
+(defparameter project-path (uiop/os:getcwd))
 
-(defparameter exec-name
-  (if (equalp :OS-WINDOWS (uiop/os:detect-os))
-      "execnik.exe"
-      "execnik"))
-
-(push
- project-path
- asdf:*central-registry*)
-
+;;; load the project
+(push project-path asdf:*central-registry*)
 (ql:quickload :execnik)
+
+;;; change to the project's package
 (in-package :execnik)
 
+;;; depending on OS choose compilation options
 (if (equalp :OS-WINDOWS (uiop/os:detect-os))
+    ;; Windows
     (sb-ext:save-lisp-and-die
      (merge-pathnames common-lisp-user::project-path
-                      common-lisp-user::exec-name)
+                      "execnik.exe")
      :toplevel #'run
      :executable T
      :application-type :gui)            ; windows may need this one
 
+    ;; Linux
     (sb-ext:save-lisp-and-die
      (merge-pathnames common-lisp-user::project-path
-                      common-lisp-user::exec-name)
+                      "execnik")
      :toplevel #'run
      :executable T))
