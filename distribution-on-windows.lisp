39b2e810a265cc746ba52b77755b28fa012a643c
Jacek Podkanski <ruby.object@googlemail.com>
2019-08-17 13:17:50 +0100

    beginning of work on compiling executable distribution

---
+++ distribution-on-windows.lisp
@@ -0,0 +1,33 @@
+;;; sbcl --load THIS-FILE
+
+(declaim (optimize (space 0) (speed 0) (debug 3)))
+
+(ql:quickload '(:external-program
+                :cl-strings))
+
+(defpackage #:distribution-on-windows
+  (:use #:cl))
+
+(in-package #:distribution-on-windows)
+
+(defparameter project-path (uiop/os:getcwd))
+
+(format t "project path is ~A~%" project-path)
+
+(defparameter *output*
+  (with-output-to-string (out)
+    ;; run command with parameters as list of strings
+    (external-program:run "Listdlls64.exe" '("execnik.exe")
+                          :output out)))
+(if  (search "No matching processes were found" *output*)
+     (format t "~%~%Please start execnik.exe and do not close until we find used libraries")
+     (format t "~%~%Going to read the libraries used"))
+
+(defparameter mingw-list
+  (let ((string-list (uiop:split-string *output*
+                                        :separator '(#\Return #\Newline)))
+        (preceding-path-length 10))
+    (loop for e in string-list
+          for found-index = (search "mingw64" e)
+          when found-index
+            collect (subseq e (- found-index preceding-path-length)))))
