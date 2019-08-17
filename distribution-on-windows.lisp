291447a26ec4d2a38729a25d304b867a656204a1
Jacek Podkanski <ruby.object@googlemail.com>
2019-08-17 22:24:55 +0100

    some progress

--- distribution-on-windows.lisp
+++ distribution-on-windows.lisp
@@ -10,6 +10,7 @@
 
 (in-package #:distribution-on-windows)
 
+(defparameter mingw-path #p "c:/msys64/mingw64/")
 (defparameter project-path (uiop/os:getcwd))
 
 (format t "project path is ~A~%" project-path)
@@ -33,9 +34,33 @@
             collect (subseq e (- found-index preceding-path-length)))))
 
 
-(external-program:run "mkdir"  '("distro") :output *standard-output*)
-(external-program:run "mkdir"  '("distro/bin") :output *standard-output*)
-(external-program:run "cp"  '("execnik.exe" "distro/bin/") :output *standard-output*)
+(external-program:run "mkdir"  '("distro"))
+(external-program:run "mkdir"  '("distro/bin"))
+(external-program:run "cp"  '("execnik.exe" "distro/bin/"))
 
 (loop for dll in  mingw-list do
   (external-program:run "cp" (list dll "distro/bin")))
+
+(external-program:run "mkdir"  '("distro/share"))
+(external-program:run "mkdir"  '("distro/share/glib-2.0"))
+(external-program:run "mkdir"  '("distro/share/glib-2.0/schemas"))
+
+(external-program:run "mkdir"  '("distro/share/icons"))
+(external-program:run "mkdir"  '("distro/share/icons/Adwaita"))
+(external-program:run "mkdir"  '("distro/share/icons/hicolor"))
+
+(defun source-wild-path (project wild path )
+  (let ((cs (if wild
+                "~A~A/*"
+                "~A~A/")))
+      (format nil cs project path)))
+
+(external-program:run "cp" (list (source-wild-path mingw-path T "share/glib-2.0/schemas")
+                                 (source-wild-path project-path nil "distro/share/glib-2.0/schemas")))
+(external-program:run "cp" (list (source-wild-path mingw-path T "share/icons/Adwaita")
+                                 (source-wild-path project-path nil "distro/icons/Adwaita")))
+(external-program:run "cp" (list (source-wild-path mingw-path T "share/icons/hicolor")
+                                 (source-wild-path project-path nil "distro/icons/hicolor")))
+
+(format t "~&Done!~%")
+(sb-ext:exit)
